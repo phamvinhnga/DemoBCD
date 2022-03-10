fb3bbca96848893e306ad6eeede20dc149739297
phamvinhnga <40154445+phamvinhnga@users.noreply.github.com>
2022-03-10 14:01:38 +0700

    origin/feature/add-automapper

---
+++ Demo.Service/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿
+using AutoMapper;
+using Demo.EntityFramework.Entities;
+using Demo.Service.Dtos;
+
+namespace Demo.Service.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<OrganizationInputDto, Organization>().ForMember(f => f.Titles, a => a.Ignore());
+            CreateMap<TitleInputDto, Title>();
+            CreateMap<UserOrganizationInputDto, UserOrganization>();
+        }
+    }
+}
